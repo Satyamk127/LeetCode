de7459b9cd2be6d449536c42d1a50f612158575b
Satyam keshari <153521543+Satyamk127@users.noreply.github.com>
2026-04-22 19:08:38 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (53.55%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -1,15 +1,27 @@
 
 class Solution {
-    public void find( TreeNode root,ArrayList<Integer>arr){
-        if(root==null) return ;
-        find(root.left,arr);
-        arr.add(root.val);
-        find(root.right,arr);
+
+     public void inverseOrder(TreeNode root, int k, int [] res, int [] cout ){
+        if(root==null || cout[0]>=k){
+            return ;
+        }
+        inverseOrder(root.left, k,res,cout);
+        cout[0]++;
+        if(cout[0]==k){
+            res[0] = root.val;
+            return ;
+        }
+        inverseOrder(root.right, k,res,cout);
+        
+        
     }
     public int kthSmallest(TreeNode root, int k) {
-        ArrayList<Integer> ans = new ArrayList<>();
-        find(root,ans);
-        return ans.get(k-1);
+
+        int [] result = {-1};  // index o per -i initialaized kiya hai 
+        int [] count = {0};
+        
+        inverseOrder(root, k, result, count);
+        return result[0];
         
     }
 }
