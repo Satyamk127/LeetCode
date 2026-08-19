2413617aaacf3ddab8cdf9910b95bbf546ff15f8
Satyam keshari <153521543+Satyamk127@users.noreply.github.com>
2026-08-20 00:10:48 +0530

    Time: 640 ms (90.88%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,6 +1,6 @@
-with satyam as(select d.name as Department, e.name as Employee ,e.salary as Salary,
-dense_rank()  over(partition by d.name order by e.salary desc ) as r
-from Employee as e join Department as d
-on e.departmentId = d.id)
-select Department ,Employee ,Salary from satyam
-where r=1;
+select Department, Employee, Salary from (select d.name as Department,e.name as Employee ,e.salary as Salary ,
+rank() over(partition by d.name order by e.salary desc) as salary_rank
+from employee as e 
+join Department as d on e.departmentId=d.id
+) r
+where salary_rank=1;
